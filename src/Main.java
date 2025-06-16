91f86085bc10d4b4348efe698bffd1ebd3761fbf
whals <whalsdud0131@gmail.cm>
2025-06-16 10:45:49 +0900

    10:45

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
-public class Main {
-    static int count = 10;
-    public static void main(String[] args) {
+class Person {
+    public static void greet() {
+        System.out.println("Hello from static method!");
+    }
 
+    public void sayHello() {
+        System.out.println("Hello from instance method!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Person.greet(); // 인스턴스 없이 호출 가능
+
+        Person p = new Person();
+        p.sayHello();
     }
 }
